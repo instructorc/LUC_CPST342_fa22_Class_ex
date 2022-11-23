4e8810c3cba257290b10c44eb735a37da4dd6d33
C. Fulton <cfulton@luc.edu>
2022-11-22 21:27:00 -0600

    add axios example

--- axios_ex/makerequest.js
+++ axios_ex/makerequest.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 //import { axios } from "axios";
 
 
-
   axios.get('https://603a806df1d6aa0017a10c48.mockapi.io/users')
   .then((response) => {
     console.log(response.data);
